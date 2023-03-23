a577678fa631b5b76f269db16e84e1342c69c44b
smartan38 <skspl38@gmail.com>
2023-03-23 15:04:10 +0530

    initialize commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import session from "express-session"
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import cors from "cors"
+
 
 const app = express(); 
 export default app;
@@ -18,30 +18,30 @@ dotenv.config({
     secret :process.env.SESSION_SECRET,
     resave : false,
     saveUninitialized :false,
-    cookie:{
-      secure : process.env.NODE_ENV==="development"?false:true,
-      httpOnly : process.env.NODE_ENV==="development"?false:true,
-      sameSite : process.env.NODE_ENV==="development"?false:"none",
+    // cookie:{
+    //   secure : process.env.NODE_ENV==="development"?false:true,
+    //   httpOnly : process.env.NODE_ENV==="development"?false:true,
+    //   sameSite : process.env.NODE_ENV==="development"?false:"none",
 
-    },
+    // },
   }))
-  app.use(cookieParser())
-  app.use(express.json())
-  app.use(urlencoded({
-    extended :true,
-  }))
+  // app.use(cookieParser())
+  // app.use(express.json())
+  // app.use(urlencoded({
+  //   extended :true,
+  // }))
 
-  app.use(cors({
-    credentials:true,
-    origin :process.env.FRONTEND_URL,
-    methods :["GET","POST","PUT","DELETE"],
+  // app.use(cors({
+  //   credentials:true,
+  //   origin :process.env.FRONTEND_URL,
+  //   methods :["GET","POST","PUT","DELETE"],
 
-  }))
+  // }))
  
   app.use(passport.authenticate("session"))
   app.use(passport.initialize())
   app.use(passport.session())
-  app.enable("trust proxy")
+  // app.enable("trust proxy")
 connectPassport()
 
 // importing routes
