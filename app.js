ce977abdb33901940eb26a9bd92ce7f56b2f2560
smartan38 <skspl38@gmail.com>
2023-03-24 15:00:22 +0530

    after fixing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import session from "express-session"
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-
+import cors from "cors";
 
 const app = express(); 
 export default app;
@@ -18,25 +18,25 @@ dotenv.config({
     secret :process.env.SESSION_SECRET,
     resave : false,
     saveUninitialized :false,
-    // cookie:{
-    //   secure : process.env.NODE_ENV==="development"?false:true,
-    //   httpOnly : process.env.NODE_ENV==="development"?false:true,
-    //   sameSite : process.env.NODE_ENV==="development"?false:"none",
+    cookie:{
+      secure : process.env.NODE_ENV==="development"?false:true,
+      httpOnly : process.env.NODE_ENV==="development"?false:true,
+      sameSite : process.env.NODE_ENV==="development"?false:"none",
 
-    // },
+    },
   }))
-  // app.use(cookieParser())
-  // app.use(express.json())
-  // app.use(urlencoded({
-  //   extended :true,
-  // }))
+  app.use(cookieParser())
+  app.use(express.json())
+  app.use(urlencoded({
+    extended :true,
+  }))
 
-  // app.use(cors({
-  //   credentials:true,
-  //   origin :process.env.FRONTEND_URL,
-  //   methods :["GET","POST","PUT","DELETE"],
+  app.use(cors({
+    credentials:true,
+    origin :process.env.FRONTEND_URL,
+    methods :["GET","POST","PUT","DELETE"],
 
-  // }))
+  }))
  
   app.use(passport.authenticate("session"))
   app.use(passport.initialize())
--- controllers/user.js
+++ controllers/user.js
@@ -11,11 +11,11 @@ export const logout=(req,res,next)=>{
     req.session.destroy((err)=>{
        if(err) return next(err)
        res.clearCookie("connect.sid",
-      //  {
-      //    secure : process.env.NODE_ENV==="development"?false:true,
-      //    httpOnly : process.env.NODE_ENV==="development"?false:true,
-      //    sameSite : process.env.NODE_ENV==="development"?false:"none",
-      //  }
+       {
+         secure : process.env.NODE_ENV==="development"?false:true,
+         httpOnly : process.env.NODE_ENV==="development"?false:true,
+         sameSite : process.env.NODE_ENV==="development"?false:"none",
+       }
        ) 
        res.status(200).json({
         message : "logged out"
