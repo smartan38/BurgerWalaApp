c28f1fba04733b053e8e82ccd6b0d1b34989dba9
smartan38 <skspl38@gmail.com>
2023-03-25 10:27:38 +0530

    initial commit

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.get("/googlelogin",passport.authenticate("google",{
 }))
 router.get("/login" , passport.authenticate("google" ,{
     successRedirect : process.env.FRONTEND_URL,
-})
+})  
 
 )
 router.get("/me",isAuthenticated,myProfile)
