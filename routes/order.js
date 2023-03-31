94ba4f11d75dffa53eeda182488a7ee9c81262b0
SAURAV KUMAR <114302903+smartan38@users.noreply.github.com>
2023-03-31 14:04:54 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { getAdminOrders, getMyOrders, getOrderDetails,paymentVerification, placeOrder, placeOrderOnline, processOrder } from "../controllers/order.js";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
+import { Contact } from "../models/User.js";
 
 const router=express.Router();
 
@@ -12,4 +13,16 @@ router.get("/order/:id", isAuthenticated,getOrderDetails)
 // add admin middleware for access to admin only
 router.get("/admin/orders", isAuthenticated,authorizeAdmin , getAdminOrders)
 router.get("/admin/order/:id", isAuthenticated,authorizeAdmin, processOrder)
+router.post("/contact",(req,res,next)=>{
+  const feedback=new Contact({
+    name : req.body.name,
+    email :req.body.email,
+    message : req.body.message})
+  feedback.save();
+     .then(data =>{
+        res.json(data)
+    }).catch(err=>{
+        res.json(err)
+    })
+})
 export default router;
