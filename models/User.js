092d5a03ef909cd3f144a84fd4e099e5dbdf8d02
SAURAV KUMAR <114302903+smartan38@users.noreply.github.com>
2023-03-31 13:54:00 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -22,3 +22,23 @@ const schema=new mongoose.Schema({
 })
 
 export const User = mongoose.model("User",schema)
+
+const schema = new mongoose.Schema({
+    name :{
+        type: String,
+        require : true,
+    },
+    email :{
+      type : String,
+      require : true,
+},
+    message : {
+        type : String ,
+        require : true,
+    }
+})
+
+export const Contact =mongoose.model("Contact",schema)
+        
+    
+    
