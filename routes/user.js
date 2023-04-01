f8d3710b9457aefcb601adb46dbdc39c34d8dd04
SAURAV KUMAR <114302903+smartan38@users.noreply.github.com>
2023-04-01 12:05:00 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.get("/googlelogin",passport.authenticate("google",{
 router.get("/login" , passport.authenticate("google" ,
                                             {
        
-    successRedirect :"http://localhost:3000",
+    successRedirect :process.env.FRONTEND_URL,
 }
 )  
 
